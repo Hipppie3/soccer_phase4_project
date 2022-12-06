ed159d10c229577811affdaccece8400f7b2761b
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2022-12-06 13:03:37 -0700

    Form

---
+++ frontend/src/Signup.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+function SignUp({ setUser }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/api/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        password_confirmation: passwordConfirmation,
+      }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1 className="signup">Sign Up</h1>
+        <input
+          className="input1"
+          placeholder="Enter Username"
+          type="text"
+          id="username"
+          autoComplete="off"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          placeholder="Enter Password"
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+        />
+        <input
+          placeholder="Re-enter Password"
+          type="password"
+          id="password_confirmation"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+          autoComplete="current-password"
+        />
+        <button className="formBtn" type="submit">KICK START NOW</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
