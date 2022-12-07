635f3ce4d20951b508964ea3f7e70c74cf77be6b
Mazin Idris <mazin1231@hotmail.com>
2022-12-06 17:50:58 -0700

    player

---
+++ frontend/components/Player.jsx
@@ -0,0 +1,36 @@
+import React, { useContext, useEffect, useState }  from "react";
+import { UserContext } from "../context/context";
+import NavBar from "./NavBar";
+import PlayerCard from "./PlayerCard";
+
+const Player = () => {
+    const [player, setPlayer] = useState([]);
+    
+    const currentUser = useContext(UserContext)
+    
+    
+
+    useEffect(() => {
+        fetch("/api/players").then((r) => {
+          if (r.ok) {
+            r.json().then((player) => setPlayer(player));
+          }
+        });
+      }, []);
+   
+    const PlayerCards = () => {
+        return ((player.length > 0) ? player.map((i) => (<PlayerCard key={i.id} players={i}/>)) : <div></div>)
+    }  
+
+
+    return ( 
+    <>
+        <NavBar/>
+        <div class="grid grid-flow-row grid-cols-4 gap-x-4 gap-y-4 bg-indigo-50">
+            {PlayerCards()}
+            
+        </div>
+    </>)
+}
+
+export default Player;
